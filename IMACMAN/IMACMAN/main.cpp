ca16c408237439f71425c0af6f310bf1c1573973
Boisier <valentin@dufois.fr>
2017-11-19 22:36:04 +0100

    First commit with base Xcode project

---
+++ IMACMAN/IMACMAN/main.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  IMACMAN
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
